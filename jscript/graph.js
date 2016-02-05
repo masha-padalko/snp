e81a67ebd6662423b2ece9872baaa5587ce40a5d
Padalko <MPadalko@kiev.luxoft.com>
2016-02-05 18:15:50 +0200

    bootsrap slider

--- jscript/graph.js
+++ jscript/graph.js
@@ -21,6 +21,12 @@ $(document).ready(function() {
 		$(this).attr('old-value', oldVal);
 	});
 
+	// Instantiate a slider
+	var mySlider = $("#ex1Slider").slider();
+
+	// Call a method on the slider
+	var value = mySlider.slider('getValue');
+
 
 	var	params = "{ 'inputStructure': { 'ifa': 4, 'economy': { 'score': 3, 'overwrite': true, 'eca1': 0, 'eca2': 0, 'eca3': 0, 	'eca4': 0, 	'eca5': 0, 	'eca6': 0 },'management': { 'score': 3, 'overwrite': true, 'fma1': 3, 'fma2': 3, 'fma3': 3, 'fma4': 3, 'fma5': 3, 'fmtr': true }, 'budgetFlexibility': { 'score': 3, 'overwrite': true, 'bfa1': 0, 'bfa2': 0, 'bfa3': 0, 'bfa4': 0, 'bfa5': 0, 'bfa6': 0 }, 'budgetPerformance': { 'score': 3, 'overwrite': true, 'bpa1': 0, 'bpa2': 0, 'bpa3': 0, 'bpa4': 0, 'bpa5': 0 }, 'liquidity': { 'score': 3, 'overwrite': true, 'lqExtAdj': 3, 'lqa1': 0, 'lqa2': 0, 'lqa3': 0, 'lqa4': 0 }, 'debt': { 'score': 3, 'overwrite': true, 'dba1': 0, 'dba2': 0, 'dba3': 0, 'dba4': 0 }, 'contLiability': 3, 'overrides': { 'cwp': 0, 'cir': 0, 'np1': 0, 'np2': 0, 'np3': 0, 'nr1': 0, 'ns1': 0 } }}"
 
